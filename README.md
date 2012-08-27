646086f837eb0a392223b7ba36124901bd7a23f5
Tima Maslyuchenko <n3imo0o@gmail.com>
2012-08-27 15:36:16 +0300

    added plugin and README

---
+++ README.md
@@ -0,0 +1,1 @@
+JQuery overtext plugin
---
+++ jquery.overtext.js
@@ -0,0 +1,44 @@
+(function($) {
+    $.fn.overtext2 = function(params) {
+        var defaults = {
+            overClass: ''
+        };
+        
+        var options = $.extend(defaults, params);
+        
+        return this.each(function() {
+            var label = $(this);
+            var f = label.attr('for');
+            
+            this.hide = function() {
+                label.hide();
+             };
+              
+             this.show = function() {
+                 if (input.val() == '') label.show();
+             };
+              
+             this.focus = function() {
+                  input.focus();
+             };
+             
+             if (f) {
+                 //id with : symbol bug fix
+                 var input = $(document.getElementById(f));
+                 
+                 input
+                    .unbind('focus', this.hide)
+                    .focus(this.hide);
+                 input
+                    .unbind('blur', this.show)
+                    .blur(this.show);
+                 label
+                    .addClass(options.overClass)
+                    .unbind('click', this.focus)
+                    .click(f, this.focus);
+
+                 if (input.val() != '') this.hide(); 
+             }
+        });
+    };
+})(jQuery);
